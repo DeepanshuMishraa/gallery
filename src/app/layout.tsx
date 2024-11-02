7d46ae2b5b0ebe2e1e1caa1f376f6a5668e3d977
Deepanshu Mishra <d4deepanshu723@gmail.com>
2024-11-02 16:55:42 +0530

    Added Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,8 @@ import TopNav from "~/components/Navbar";
 import { NextSSRPlugin } from "@uploadthing/react/next-ssr-plugin";
 import { extractRouterConfig } from "uploadthing/server";
 import { ourFileRouter } from "./api/uploadthing/core";
+import { Toaster } from "~/components/ui/toaster";
+import { CSPostHogProvider } from "./_analytics/provider";
 
 export const metadata: Metadata = {
   title: "Gallery",
@@ -24,23 +26,26 @@ export default function RootLayout({
 }>) {
   return (
     <ClerkProvider>
-      <html lang="en" className={`${GeistSans.variable}`}>
-        <NextSSRPlugin
-          /**
-           * The `extractRouterConfig` will extract **only** the route configs
-           * from the router to prevent additional information from being
-           * leaked to the client. The data passed to the client is the same
-           * as if you were to fetch `/api/uploadthing` directly.
-           */
-          routerConfig={extractRouterConfig(ourFileRouter)}
-        />
-        <body className="flex flex-col gap-4">
-          <TopNav />
-          {children}
-          {modal}
-          <div id="modal-root" />
-        </body>
-      </html>
+      <CSPostHogProvider>
+        <html lang="en" className={`${GeistSans.variable}`}>
+          <NextSSRPlugin
+            /**
+             * The `extractRouterConfig` will extract **only** the route configs
+             * from the router to prevent additional information from being
+             * leaked to the client. The data passed to the client is the same
+             * as if you were to fetch `/api/uploadthing` directly.
+             */
+            routerConfig={extractRouterConfig(ourFileRouter)}
+          />
+          <body className="flex flex-col gap-4">
+            <TopNav />
+            {children}
+            {modal}
+            <div id="modal-root" />
+            <Toaster />
+          </body>
+        </html>
+      </CSPostHogProvider>
     </ClerkProvider>
   );
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,10 +2,12 @@
 
 import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
 import { useRouter } from "next/navigation";
+import {  useToast } from "~/hooks/use-toast";
 import { UploadButton } from "~/utils/uploadthing";
 
 export default function TopNav() {
     const router = useRouter();
+    const { toast } = useToast()
   return (
     <nav className="flex w-full items-center justify-between border-b p-4 text-xl font-semibold">
       <div>Gallery</div>
@@ -19,7 +21,18 @@ export default function TopNav() {
             <UploadButton
               endpoint="imageUploader"
               onClientUploadComplete={(res) => {
+                toast({
+                  title: "Image Uploaded",
+                  description: "Your image has been uploaded",
+                });
                 router.refresh();
+              }}
+              onUploadBegin={()=>{
+                toast({
+                    title:"Uploading Image",
+                    description:"Please wait while we upload your image",
+                    duration:100000,
+                })
               }}
               onUploadError={(error: Error) => {
                 // Do something with the error.
