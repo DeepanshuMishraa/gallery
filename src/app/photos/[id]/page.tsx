8b92673848346436f9c749ba9e700ca38d47875e
Deepanshu Mishra <d4deepanshu723@gmail.com>
2024-11-03 00:35:17 +0530

    Delete Function server

--- src/app/photos/[id]/page.tsx
+++ src/app/photos/[id]/page.tsx
@@ -1,5 +1,7 @@
 import Image from "next/image";
-import { getImage } from "~/server/queries";
+import { redirect } from "next/navigation";
+import { Button } from "~/components/ui/button";
+import { DeleteImage, getImage } from "~/server/queries";
 
 export default async function PhotoModal({
   params: { id: photoId },
@@ -8,11 +10,19 @@ export default async function PhotoModal({
 }) {
 
     const image = await getImage(photoId);
+
   return(
     <div className="flex justify-center items-center h-screen w-full">
       <div className="bg-gray-800 rounded-lg p-4">
         <Image width={480} style={{objectFit:"contain"}} height={480} src={image.url} alt={image.name} className="h-96 w-96 object-cover" />
         <div className="text-center">{image.name}</div>
+        <form className="p-8" action={async()=>{
+            "use server"
+            DeleteImage(photoId)
+            redirect("/")
+        }}>
+            <Button variant="destructive">Delete Image</Button>
+        </form>
       </div>
     </div>
   )
--- src/server/queries.ts
+++ src/server/queries.ts
@@ -33,3 +33,27 @@ export async function getImage(id:string){
 
     return image;
 }
+
+
+export async function DeleteImage(id:string){
+    const user = await auth();
+
+    if(!user.userId) throw new Error("Unauthorized");
+
+    const image = await db.image.findFirst({
+        where:{
+            id:id ,
+        }
+    })
+    if(!image) throw new Error("Image Not Found");
+
+    if(image.userId !== user.userId) throw new Error("Unauthorized");
+
+    await db.image.delete({
+        where:{
+            id:id
+        }
+    })
+
+    return image;
+}
